e5dc515a6dd4a9fe327bc957be9ef0baddc73b0a
minji <flowingmj@gmail.com>
2018-08-31 16:08:39 +0900

    [Modify] compelete movie_app

--- 0_movie_app/README.md
+++ 0_movie_app/README.md
@@ -15,3 +15,37 @@
 ### react 프로젝트를 시작할 때 필요한 건
 - 컴포넌트 디자인
 - 컴포넌트의 포함관계, 개별 컴포넌트의 구성 등을 먼저 설계
+
+### prop-types 
+- 받아온 데이터(props)의 값을 검증하기 위해 사용하는 prop-types
+- props의 종류와 타입 선언하여 개발환경에서의 버그 잡음
+
+### life cycle 
+- Render : `componentWillMount()` -> `render()` -> `componentDidMount()`
+- Update : `componentWillReceiveProps()` -> `shouldComponentUpdate()` -> `componentWillUpdate()` -> `render()` -> `componentDidUpdate()`
+
+### class component(Smart) vs. functional component(Dumb) 
+- **class component** : State O / Props O
+- **functional component** : State X / Props O   
+  - lifecycle 존재X
+  - propTypes를 사용 할 땐,
+    ```js
+      function Movies () {
+        //
+      }
+
+      Movies.propTypes = {
+        title: PropTypes.string.isRequired,
+        poster: PropTypes.string.isRequired
+      }  
+    ```
+
+### AJAX on React
+#### fetch vs promise
+
+### async await
+async : 비동기
+await : 해당 함수가 끝나기를 기다리는 것(성공여부 판단x)
+
+### react-lines-ellipsis
+[LINK](https://www.npmjs.com/package/react-lines-ellipsis)
--- 0_movie_app/movie_app/src/Movie.js
+++ 0_movie_app/movie_app/src/Movie.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis'
 import './Movie.css';
 
 class Movie extends Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    summary : PropTypes.string.isRequired
   }
   render() {
     return (
-      <div>
-        <p>{this.props.title}</p>
-        <MoviePoster poster={this.props.poster} title={this.props.title} />
+      <div className='Movie'>
+        <div className='Movie__Column'>
+          <MoviePoster poster={this.props.poster} title={this.props.title} />
+        </div>
+        <div className='Movie__Column'>
+          <h1>{this.props.title}</h1>
+          <div className='Movie__Genres'>{this.props.genres.join(' ')}</div>
+          <p className='Movie_Summary'>
+            <LinesEllipsis
+              text={this.props.summary}
+              maxLine='3'
+              ellipsis='...'
+              trimRight
+              basedOn='letters'
+              />   
+          </p>
+        </div>
+        
+
       </div>
     );
   }
@@ -19,12 +38,12 @@ class Movie extends Component {
 
 class MoviePoster extends Component {
   static propTypes = {
-    title: PropTypes.string,
-    poster: PropTypes.string
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
   }
   render() {
     return (
-      <img src={this.props.poster} alt={this.props.title} />
+      <img className='Movie__Poster' src={this.props.poster} alt={this.props.title} title={this.props.title} />
     );
   }
 }
