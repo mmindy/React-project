42d22c404559af9e6155ce90bfba86046cf96663
minji <flowing_mj@naver.com>
2018-11-20 11:27:06 +0900

    repackage js code

--- elevator/js/script_es5.js
+++ elevator/js/script_es5.js
@@ -1,17 +1,18 @@
 var Elevator = (function() {
-  var WRAPPER;
   var ELEVATOR = [];
   var FLOOR;
+  var container;
 
   var init = info => {
-    WRAPPER = document.querySelector(info.id);
+    container = document.querySelector(info.id);
 
     if ( info.elevator > 0 ) {
       var i = 0
       do {
         ELEVATOR.push({
           idx : i++,
-          current_floor : 0
+          current_floor : 0,
+          prev_floor : 0
         })
       } while ( i < info.elevator )
     } else {
@@ -19,41 +20,44 @@ var Elevator = (function() {
       return false;
     }
 
-    if ( info.floor > 0 ) FLOOR = info.floor;
+    if ( info.floor > 0 ) FLOOR = info.floor - 1;
     else {
       warn("floor 잘못 입력함");
       return false;
     }
 
     setElevatorPosition();
-    addEvent();
+    addClickEvent();
   }
 
-  var addEvent = function() {
-    var floorBtn = WRAPPER.querySelectorAll(".floor .moveFloor");
+  var addClickEvent = function() {
+    var floorBtn = container.querySelectorAll(".floor .moveFloor");
+
     floorBtn.forEach(function(v, i) {
       floorBtn[i].addEventListener("click", function() {
         var goFloor = floorBtn[i].parentNode.children[0].value - 1 ;
-        
+        clickFloor = Math.abs(i - FLOOR);
+
         if (!isNaN(goFloor)) {
-          var moveMachineIdx = compareFloor(i);
-          changeCurrentFloor(moveMachineIdx, goFloor);
-          addClass(moveMachineIdx);
-          setElevatorPosition();
+          var moveElevatorIdx = selectElevator();
+          compareFloor(moveElevatorIdx, clickFloor);
+          addClass(moveElevatorIdx);
+          changeCurrentFloor(moveElevatorIdx, goFloor);
+          setElevatorPosition(moveElevatorIdx);
         }
       })
     })
   }
 
-  var compareFloor = function(clickFloor) {
-    var compareData = ELEVATOR.slice();
-    clickFloor = Math.abs(clickFloor - FLOOR);
-
-    compareData.forEach(function (v) {
+  var compareFloor = function(moveElevatorIdx,clickFloor) {
+    ELEVATOR.forEach(function (v, i) {
+      if (i === moveElevatorIdx) v.prev_floor = clickFloor;
       v.compare_floor = Math.abs(Number(v.current_floor) - Number(clickFloor));
     })
-    
-    var minData = compareData.reduce( function (previous, current) { 
+  }
+
+  var selectElevator = function() {
+    var minData = ELEVATOR.reduce( function(previous, current) { 
       if (previous.compare_floor > current.compare_floor) {
         return current;
       } else {
@@ -68,30 +72,39 @@ var Elevator = (function() {
     return minData.idx;
   }
 
-  var changeCurrentFloor = function(moveMachineIdx, goFloor) {
+  var addClass = function(idx) {
+    var elevatorList = container.querySelectorAll(".movement .machine");
+    elevatorList.forEach(function(v) {
+      v.classList.remove("active");
+    })
+    elevatorList[idx].classList.add("active");
+  }
+
+  var setElevatorPosition = function() {
+    setBottomPosition("prev_floor");
+
+    setTimeout(function() {
+      setBottomPosition("current_floor");
+    }, 1000)
+  };
+
+  var setBottomPosition = function(floorName) {    
+    var floorH = Number(container.querySelector(".floor").offsetHeight + 1);
+    var elevator = container.querySelectorAll(".machine .ele");
+    
+    elevator.forEach( function(v, i) {
+      if (floorName === "current_floor") v.style.bottom = (ELEVATOR[i].current_floor * floorH) + "px";
+      else v.style.bottom = (ELEVATOR[i].prev_floor * floorH) + "px";      
+    })
+  }
+
+  var changeCurrentFloor = function(moveElevatorIdx, goFloor) {
     ELEVATOR.some(function(v,i) {
-      if ( v.idx === moveMachineIdx) {
+      if ( v.idx === moveElevatorIdx) {
         v.current_floor = goFloor;
         return true;
       }
     });
-  };
-
-  var addClass = function(idx) {
-    var machineList = WRAPPER.querySelectorAll(".movement .machine");
-    machineList.forEach(function(v) {
-      v.classList.remove("active");
-    })
-    machineList[idx].classList.add("active");
-  }
-
-  var setElevatorPosition = function() {
-    var floorH = Number(WRAPPER.querySelector(".floor").offsetHeight);
-    var movement = WRAPPER.querySelectorAll(".machine .ele");
-
-    movement.forEach( function(v, i) {
-      v.style.bottom = (ELEVATOR[i].current_floor * floorH) + "px";
-    })
   };
 
   return function(info){
